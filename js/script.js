930b2dd35788c023f097afb232f8eabb4a39cbab
Kory Telesford <tkory85@gmail.com>
2018-12-12 22:52:51 +0000

    WIP Html and JavaScript being modified

--- js/script.js
+++ js/script.js
@@ -9,6 +9,40 @@ $(document).ready(function() {
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
         var totalScore;
+        var Yes;
+        var No;
+        
+        if (q1Result == Yes) {
+            console.log("Q1 Yes");
+            
+            
+        } else if (q1Result == No) {
+            console.log("Q1 No");
+        
+        
+        }
+        
+        if (q2Result == Yes) {
+            console.log("Q2 Yes");
+            
+            
+        } else if (q2Result == No) {
+            console.log("Q2 No");
+        
+        
+        }
+        
+        if (q3Result == Yes) {
+            console.log("Q3 Yes");
+            
+            
+        } else if (q3Result == No) {
+            console.log("Q3 No");
+        
+        
+        }
+        
+        
     });
 
 });
